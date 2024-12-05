38e86f6e8b4c8ed02299ecdbb1628a82ac15c036
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2024-12-05 20:58:23 +0530

    Update Move pieces to obtain a string.java

--- DAY_2/Move pieces to obtain a string.java
+++ DAY_2/Move pieces to obtain a string.java
@@ -1,1 +1,29 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        //Let's apply 2 pointers approach(not actually)- in 2 diff strings
+        int i=0,j=0;
+        int n=start.length();
 
+        while(i<n || j<n)
+        {
+            //get letters
+            while(i<n && start.charAt(i)=='_') i++;
+            while(j<n && target.charAt(j)=='_') j++;
+
+            //count is same if both the string end at the same time
+            if(i==n || j==n)
+                return (i==n && j==n);
+            
+            //check false conditions
+            if((start.charAt(i)!=target.charAt(j)) ||
+               (start.charAt(i)=='L' && j>i) ||
+               (start.charAt(i)=='R' && j<i)
+              )
+              return false;
+            
+            i++;
+            j++;
+        }
+        return true;//if whole loop executed successfully means false conditions never became true: & false was not returned
+    }
+}
