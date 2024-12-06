fcfed45e9197a594dfdacb5e1fd041c791006a65
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2024-12-07 00:52:24 +0530

    Update Maximum Number of Integers to Choose From a Range I.java

--- DAY_3/Maximum Number of Integers to Choose From a Range I.java
+++ DAY_3/Maximum Number of Integers to Choose From a Range I.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+            HashSet<Integer> hs=new HashSet<>();
+            for(int i:banned){ hs.add(i); }
+            int sum=0,count=0;
+            for(int i=1;i<=n;i++)
+            {
+                if(!hs.contains(i))
+                {
+                    sum+=i;
+                    if(sum<=maxSum) 
+                        count++;
+                    else
+                        break;
+                }
+            }
+            return count;
+    }
+}
