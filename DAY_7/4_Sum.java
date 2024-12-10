f54400d2c811fee2f49dcfc1482f56b5e5e83933
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2024-12-11 00:04:01 +0530

    Update 4_Sum.java

--- DAY_7/4_Sum.java
+++ DAY_7/4_Sum.java
@@ -2,6 +2,8 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
 //M3: OPTIMAL(AND BEST WHAT I UNDERSTAND)-- see striver's optimal (good pointer visualization explaination)
 //4 variables(2 fixed for a loop , 2 moving like i,j,k,...,l)->k++;l--;
+//T.C=n*n*(n-3)=O(n^3)....n-3 for k running from 3rd position till last so almost n.
+//S.C=O(1)- as we're taking 0 or no space in deriving the answer(just n space in returning the answer- that's not the part of S.C)
 int n=nums.length;
 List<List<Integer>> ans=new ArrayList<>();
 Set<List<Integer>> set=new HashSet<>();
