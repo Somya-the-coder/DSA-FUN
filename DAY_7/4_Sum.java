93f46ecbe6a882fb244f0e2361b65a7df964a814
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2024-12-10 23:45:09 +0530

    Create 4_Sum.java

---
+++ DAY_7/4_Sum.java
@@ -0,0 +1,64 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+//M3: OPTIMAL(AND BEST WHAT I UNDERSTAND)-- see striver's optimal (good pointer visualization explaination)
+//4 variables(2 fixed for a loop , 2 moving like i,j,k,...,l)->k++;l--;
+int n=nums.length;
+List<List<Integer>> ans=new ArrayList<>();
+Set<List<Integer>> set=new HashSet<>();
+Arrays.sort(nums);
+for(int i=0;i<n;i++)
+{
+    if(i>0 && nums[i]==nums[i-1]) continue;
+    for(int j=i+1;j<n;j++)
+    {
+        if(j!=(i+1) && nums[j]==nums[j-1]) continue;
+        int k=j+1;
+        int l=n-1;
+        while(k<l){
+            long sum=nums[i];
+            sum+=nums[j];
+            sum+=nums[k];
+            sum+=nums[l];
+            if(sum==target)
+            {
+                set.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                k++;l--;
+            }
+            else if(sum<target) k++;
+            else l--;
+        }
+    }
+}
+// return ans.addAll(set);//-error- boolean cannot be converted to List<List<Integer>>
+ans.addAll(set);
+return ans;
+    }
+}
+
+//M2: BETTER - (WRITE FEW LINES AND CALL 3 SUM) 
+
+// //M1: BRUTEFORCE- USE HASHSET TO AVOID DUPLICATES - TLE ERROR
+//         int n = nums.length;
+//         Set<List<Integer>> resultSet = new HashSet<>(); // Use a HashSet to avoid duplicates
+        
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 for (int k = j + 1; k < n; k++) {
+//                     for (int l = k + 1; l < n; l++) {
+//                         if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
+//                             List<Integer> temp = new ArrayList<>();
+//                             temp.add(nums[i]);
+//                             temp.add(nums[j]);
+//                             temp.add(nums[k]);
+//                             temp.add(nums[l]);
+//                             temp.sort(null); // Ensure the list is in sorted order
+//                             resultSet.add(temp); // Add to the set to prevent duplicates
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return new ArrayList<>(resultSet); // Convert the set to a list before returning
+//     }
+// }
