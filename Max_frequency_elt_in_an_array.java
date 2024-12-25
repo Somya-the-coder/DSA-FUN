0b8338d5a9caf05dab4aff6c361923b30a304238
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2024-12-25 06:31:23 +0530

    Create Max_frequency_elt_in_an_array.java

---
+++ Max_frequency_elt_in_an_array.java
@@ -0,0 +1,64 @@
+/*
+Alternative Approach Using Sorting
+Sort the array: Time complexity is O(n log n).
+Count frequencies in a single pass: Time complexity is O(n).
+*/
+
+//HASHMAP APPROACH
+class Solution {
+    int maxRepeating(int k, int[] arr) {
+        // code here
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i:arr)
+        {
+           map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int max=0;
+        int elt=0;
+        for(Map.Entry<Integer,Integer> entry:map.entrySet())//rem both map and entry in interface name is in capital letters
+        {
+            if(entry.getValue()>max)
+            {
+                max=entry.getValue();
+                elt=entry.getKey();
+            }
+            //handling edge case
+            else if((entry.getValue() == max) && (entry.getKey()<elt))//elt here is max elt
+            {
+                elt=entry.getKey();
+            }
+            //System.out.println(entry+"##"+map.entrySet());
+            /*Above sopln prints:
+                Your Output: 
+                    1=1##[1=1, 2=3]
+                    2=3##[1=1, 2=3]  
+            */
+        }
+        return elt;
+    }
+}
+
+// Each put operation in the HashMap takes average O(1) time.
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// This analysis shows that your maxRepeating method is efficient in terms of both time and space for processing the input array.
+
+
+/*The Map.Entry Interface
+In Java, Map.Entry is an interface that provides a view of a key-value pair in a Map. It's a nested interface within the Map interface. It has a few important methods:
+
+using map.entry we get the privilege of using these 3 wonderful methods with() symbol -don't forget
+getKey(): Returns the key corresponding to this entry.
+
+getValue(): Returns the value corresponding to this entry.
+
+setValue(V value): Replaces the value corresponding to this entry with the specified value.
+
+Using entrySet()
+The entrySet() method of a Map returns a set of Map.Entry objects. This set contains all the key-value pairs in the map. You can iterate over this set to access each key-value pair in the map.
+
+SO,
+Iteration: for (Map.Entry<Integer, Integer> entry : map.entrySet())
+
+This loop iterates over each Map.Entry object in the entrySet of the map.
+*/
