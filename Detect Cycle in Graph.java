d4e727d50abc1d03bc9747f432b769c522ed4ed8
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-01-08 06:21:51 +0530

    Update Detect Cycle in Graph.java

--- Detect Cycle in Graph.java
+++ Detect Cycle in Graph.java
@@ -1,37 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class DriverClass {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-            int V = sc.nextInt();
-            int E = sc.nextInt();
-            for (int i = 0; i < V; i++) list.add(i, new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                int u = sc.nextInt();
-                int v = sc.nextInt();
-                list.get(u).add(v);
-            }
-            if (new Solution().isCyclic(V, list) == true)
-                System.out.println("1");
-            else
-                System.out.println("0");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-/*Complete the function below*/
-
 class Solution {
     
     // private boolean dfsCheck(int node,ArrayList<ArrayList<Integer>> adj, int vis[],int pathVis[])
@@ -77,8 +43,7 @@ public boolean isCyclic(int V,ArrayList<ArrayList<Integer>> adj)
     int[] inDegree=new int[V];
     Queue<Integer> q=new LinkedList<>();
     
-    //calc indeg for each node- 
-    //traverse through the adj list and each ke index par increment count
+//calculating indegree for each node- make indegree array traverse through the adj list and each ke index par increment count in the indegree array
     for(int i=0;i<V;i++)
     {
         for(int neighbor:adj.get(i))
