7c2bc68965cbb5689755fb0c4f3f7e67c6d2f2dd
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-01-13 08:12:37 +0530

    Update Stack from scratch.java

--- Stack from scratch.java
+++ Stack from scratch.java
@@ -1,1 +1,21 @@
+//STACK USING JCF
+
+import java.util.*;
+class Stack_basic
+{
+    public static void main(String args[])
+    {
+        Stack<Integer> stk=new Stack<>();//handle empty case ...12 class yaad haina
+        stk.push(20);
+        stk.push(30);
+        System.out.println(stk);//in list type form [. , .]
+        stk.pop();
+        System.out.println(stk);
+        // System.out.println(stk.pop());//EmptyStackException is thrown
+        System.out.println(stk.peek());
+
+        stk.push(90);
+        System.out.println(stk.pop());
+    }  
+}
 
