3859cfd5632bbedd4b12f854383b2865266dff6f
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-02-01 22:31:07 +0530

    Update Is_Sum Tree.java

--- Is_Sum Tree.java
+++ Is_Sum Tree.java
@@ -39,3 +39,34 @@ class Solution {
         return (root.data==sum(root.left)+sum(root.right)) && isSumTree(root.left) && isSumTree(root.right);
     }
 }
+
+
+/*
+//again for practice:-
+class Solution{
+    //simple maan lo recursion loop ka alternative hai...(fun call itself in it with increment/decrement)   + those 3 steps
+    
+    public int sum(Node root)
+    {
+        if(root==null)  return 0;
+        return root.data+sum(root.left)+sum(root.right);
+    }//recursively calls left and right and add
+    
+    boolean isSumTree(Node root)
+    {
+        // BC
+        if(root==null) return true;//no tree is a sum tree
+        if(root.left==null && root.right==null) return true;//leaf node is also a sum tree
+        //kaam bhi yahi hai sum function is being called
+        //call n-1...assuming it will call the rest nicely in depth in return itself
+        return root.data==sum(root.left)+sum(root.right) && isSumTree(root.left) && isSumTree(root.right);
+        //above line vvimportant explaination:-
+/*
+sum(root.left) + sum(root.right) → Calculates the sum of left and right subtrees.
+root.data == sum(root.left) + sum(root.right) → Checks if the current node's value is equal to this sum.
+isSumTree(root.left) && isSumTree(root.right) → Recursively verifies if the left and right subtrees are also Sum Trees.
+tc=O(n^2)...as 2 functions are called
+*/
+    }
+}
+*/
