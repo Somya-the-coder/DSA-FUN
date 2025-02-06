20f6529ba962c928a71182163c28c495ccdb35d9
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-02-06 17:49:32 +0530

    Update Reverse LL.java

--- Reverse LL.java
+++ Reverse LL.java
@@ -1,19 +1,28 @@
 class Solution {
     Node reverseList(Node head) {
-        //Iterative(easy)- 3 variables, 4 steps
-        Node prev=null;
-        Node curr=head;
+        //M1:LL-STK-LL
+ 
         
-        while(curr!=null)
-        {
-            Node next=curr.next;//write 2nd.....storing next 1st
-            curr.next=prev;//write 1st
-            prev=curr;//write 3rd
-            curr=next;//write 4th
-        }
-        
-        return prev;
+        // //M2: Iterative(easy)- 3 variables, 4 steps
+        // Node prev=null;
+        // Node curr=head;
+        // while(curr!=null)
+        // {
+        //     Node next=curr.next;//write 2nd.....storing next 1st
+        //     curr.next=prev;//write 1st
+        //     prev=curr;//write 3rd
+        //     curr=next;//write 4th
+        // }
+        // return prev;
 
-        //Recursive
+     
+        // //M3: Recursive
+        if(head==null || head.next==null)   return head;//.......striver mast expl at last video
+        Node newnode=reverseList(head.next);
+        Node front=head.next;
+        front.next=head;
+        head.next=null;
+        return newnode;
+
     }
 }
