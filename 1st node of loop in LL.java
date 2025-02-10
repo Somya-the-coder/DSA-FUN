b27951e3f6023dfb4753c278642f53bbbb8bcbd1
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-02-10 15:18:59 +0530

    Create 1st node of loop in LL.java

---
+++ 1st node of loop in LL.java
@@ -0,0 +1,46 @@
+
+// User function Template for Java
+
+/* class Node
+{
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public static Node findFirstNode(Node head) {
+        // code here
+        Node slow=head,fast=head;
+        int j=0;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow)
+            {
+                j=1;
+                break;
+            }
+        }
+        if(j==1)
+        {
+            slow=head;
+            while(slow!=fast)
+            {
+                fast=fast.next;
+                slow=slow.next;
+            }
+            return fast;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
