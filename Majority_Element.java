dd3a2b3150bf3ae1b7b63b66499c49efb2a69315
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-02-15 18:10:46 +0530

    Update Majority_Element.java

--- Majority_Element.java
+++ Majority_Element.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    static int majorityElement(int arr[]) {
+        // your code here
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            if(!map.containsKey(arr[i])) map.put(arr[i],1);
+            else    map.put(arr[i],map.get(arr[i])+1);
+        }
+        for(Map.Entry<Integer,Integer> entry: map.entrySet())
+        {
+            if(entry.getValue()>(arr.length/2))   return entry.getKey();
+        }
+        return -1;
+    }
+}
+//Time Complexity: O(N), as we traverse the array once and then the map.Space Complexity: O(N), as we store frequencies in a HashMap.
