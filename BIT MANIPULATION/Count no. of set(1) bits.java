3d4e737024f47c75213ff1ae6403f5da0267dcee
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-02-19 11:48:12 +0530

    Update Count no. of set(1) bits.java

--- BIT MANIPULATION/Count no. of set(1) bits.java
+++ BIT MANIPULATION/Count no. of set(1) bits.java
@@ -12,3 +12,12 @@ class Solution {
     }
 }
 //tc= O(log n base 2+1)...any no. can be represented in bonary as log n base 2 + 1
+//sc=O(1)
+/*
+TC...........
+n each iteration, n is right-shifted by 1 (n = n >> 1), effectively dividing it by 2.
+The number of times we can divide 𝑛 by 2 before it becomes 0 is O(logn) (base 2).
+SC..........
+We use only a few integer variables (count, n), which take constant space.
+No extra data structures (arrays, recursion stack, etc.) are used.
+*/
