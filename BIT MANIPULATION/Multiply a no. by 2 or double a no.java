eee489d4f56068b828bc5162fe9b61e10a6b299c
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-02-21 19:59:08 +0530

    Create Multiply a no. by 2 or double a no.java

---
+++ BIT MANIPULATION/Multiply a no. by 2 or double a no.java
@@ -0,0 +1,13 @@
+//Double or multiply a no. by 2 using bitwise operator
+//TC=SC=O(1)
+import java.util.*;
+class Toh
+{
+    //multiplies a no. by 2 using bitwise opeartor
+    public static void main(String args[])
+    {
+        int n=8;
+        int square=n<<1;
+        System.out.println(square);
+    }
+}
