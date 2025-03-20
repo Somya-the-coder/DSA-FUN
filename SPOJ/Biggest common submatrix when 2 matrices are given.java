802c1611a604959491103bc9f3f9ffc58214c9a7
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-03-20 20:48:23 +0530

    Update Biggest common submatrix when 2 matrices are given.java

--- SPOJ/Biggest common submatrix when 2 matrices are given.java
+++ SPOJ/Biggest common submatrix when 2 matrices are given.java
@@ -1,4 +1,10 @@
 /*
 Brute Force: Tries all squares, too slow for large cases.
 Optimized (Binary Search + Hashing): Uses row/column hashing + binary search, making it efficient.
+
+NOTE:-------------------------------
+📝 Rule of Thumb:
+
+If you're finding the largest (maximization problem) → 🔍 Binary Search!
+If you're just finding an occurrence → ❌ No need for Binary Search!
 */
