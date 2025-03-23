876b1bcb4db90f1c69c45c137e65140ab5f413a8
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-03-23 12:04:35 +0530

    Update 3.java

--- HighRadius/3.java
+++ HighRadius/3.java
@@ -1,1 +1,5 @@
-
+/*
+Problem 3:
+There are N street lights numbered from 1 to N. Each street lamp i has two numbers start(i), end(i) meaning they can
+light all the streets numbered from start to end. You just need to find out how many areas are getting lighted.
+*/
