ba515957e4bca48226192971f50698beb51a5b77
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-03-23 12:04:26 +0530

    Update 1.java

--- HighRadius/1.java
+++ HighRadius/1.java
@@ -1,1 +1,7 @@
-
+/*
+Problem 1:
+There are N bowls numbered from 1 to N and each bowl can contain maximum 9 marbles. A user has to add a marble to the last bowl i.e. Nth Bowl.
+If Nth bowl is full i.e. it already has 9 marbles, you need to check in (N-1)th bowl. If (N-1)th bowl has space to accomodate one more marble, 
+then you need to add marble in it and remove all the marbles from Nth bowl. You need to identify the bowl will be modified when you add a marble. 
+If all bowls are full, print output as 0.
+*/
