e12dacbe05467db92a074d5df36995b26478545b
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-03-25 23:28:26 +0530

    Update 2.java

--- HighRadius/2.java
+++ HighRadius/2.java
@@ -2,5 +2,7 @@
 Problem 2:
 There are N interns numbered from 1 to N. Each intern is assigned a unique ID for each day. Rules being 1<=N<=24.
 The ith intern has an ID of (5000*i) on day 1 and for the rest of the days, Day(i) = Day(i-1)+5000+(i-1)
-Input to the problem is ID of intern. Output is number of intern on particular day. 
+Input to the problem is ID of intern. Output is number of intern on particular day.
+
+
 */
