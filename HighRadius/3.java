62b7896b44df0451460462e1898da05822bb9950
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-03-27 15:47:22 +0530

    Update 3.java

--- HighRadius/3.java
+++ HighRadius/3.java
@@ -3,3 +3,31 @@ Problem 3:
 There are N street lights numbered from 1 to N. Each street lamp i has two numbers start(i), end(i) meaning they can
 light all the streets numbered from start to end. You just need to find out how many areas are getting lighted.
 */
+import java.itil.*;
+public static void main(String args[])
+{
+  Scanner in=new Scanner(System.in);
+  int N=in.nextInt();
+
+  boolean[] lit=new boolean[10001];//assuming max street no.
+
+  for(int i=0;i<N;i++)
+  {
+    int start=in.nextInt();
+    int end=in.nextInt();
+    for(int j=start;j<=end;j++)
+      {
+        lit[j]=true;
+      }
+  }
+  int count=0;
+  for(boolean b:lit)
+    {
+      if (b){
+        count++;}
+    }
+  System.out.println(count);
+  in.close();
+}
+//tc=O(N*R+M), sc=O(M).....hashset approach
+//tc=O(N*R), sc=O(M).......boolean array approach
