56ae1511a5a9623eae2c29ab161b9317000aaa2d
Somya Verma <138440801+Somya-the-coder@users.noreply.github.com>
2025-04-23 10:46:15 +0530

    Create String Reverse.java

---
+++ String Reverse.java
@@ -0,0 +1,20 @@
+lass Solution {
+    public void reverseString(char[] s) {
+
+      //in-place reversal
+    int n=s.length;
+    for(int i=0;i<n/2;i++)
+    {
+        char temp=s[i];
+        s[i]=s[n-i-1];
+        s[n-i-1]=temp;
+    }
+      //space taking dev sir reversal technique
+    //     String s1="";
+    //     for(char i:s)
+    //     {
+    //         s1=i+s1;
+    //     }
+    //     System.out.println(s1);
+    }
+}
